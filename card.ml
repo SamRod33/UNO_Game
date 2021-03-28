de013ea2902c3a1b93701cf176a5d9c1810c776a
Oluwatise Alatise <ofa2@cornell.edu>
2021-03-27 21:00:09 -0400

    Implement Card.ml and template for custom card json file. Also added ocaml formatter from A2.
    - Implemented defintions from mli
    - Changed actions from variant type to record type
    - Card type now has actions which is a record type instead of action variant list type

---
+++ card.ml
@@ -0,0 +1,60 @@
+open Yojson.Basic.Util
+
+(**************************************************************************)
+type color =
+  | R
+  | G
+  | B
+  | Y
+  | ANY
+
+type actions = {
+  skip : bool;
+  reverse : bool;
+  swap : bool;
+  change_color : bool;
+}
+
+type draw_penalty = int
+
+type t = {
+  color : color;
+  actions : actions;
+  penalty : draw_penalty;
+}
+
+(**************************************************************************)
+let j json = Yojson.Basic.from_file json
+
+let to_color s =
+  match String.lowercase_ascii s with
+  | "red" -> R
+  | "green" -> G
+  | "blue" -> B
+  | "yellow" -> Y
+  | "any" -> ANY
+  | _ -> raise (Invalid_argument s)
+
+let to_actions j =
+  {
+    skip = j |> member "skip" |> to_bool;
+    reverse = j |> member "reverse" |> to_bool;
+    swap = j |> member "swap" |> to_bool;
+    change_color = j |> member "change color" |> to_bool;
+  }
+
+let create j =
+  {
+    color = j |> member "color" |> to_string |> to_color;
+    actions = j |> member "actions" |> to_actions;
+    penalty = j |> member "draw penalty" |> to_int;
+  }
+
+(**************************************************************************)
+let draw_penalty c = c.penalty
+
+let color c = c.color
+
+let actions c = c.actions
+
+(**************************************************************************)
