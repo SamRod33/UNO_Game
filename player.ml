6c463dddeabca700a2c99b989a808ac7f2f2fa82
Keri Tenerowicz <46474707+keritenerowicz@users.noreply.github.com>
2021-03-31 16:40:25 -0400

    fixed type errors with player hand & card module

--- player.ml
+++ player.ml
@@ -4,31 +4,29 @@ exception CardNotInHand of Card.t
 
 type t = {
   name : string;
-  hand : Card.t list
+  hand : Card.t list;
 }
 
-let name = t.name
+let name p = p.name
 
-let player_hand = t.hand
+let player_hand p = p.hand
 
-let is_uno cards = 
-  match cards with
-  | [] -> true
-  | h :: [] -> true
-  | _ -> false
+let is_uno cards =
+  match cards with [] -> true | [ h ] -> true | _ -> false
 
-let add_card player card = 
-  {player with hand = card::hand}
+let add_card player card = { player with hand = card :: player.hand }
 
-(* [remove_first_of_dup hand card acc] is the player's hand after removing the
-    first of any given card form the player's original hand [hand] if the card
-    [card] is in the hand.*)
-let rec remove_first_of_dup hand card (acc : Card.t list) = 
+(* [remove_first_of_dup hand card acc] is the player's hand after
+   removing the first of any given card form the player's original hand
+   [hand] if the card [card] is in the hand.*)
+let rec remove_first_of_dup hand card acc =
   match hand with
   | [] -> hand
-  | h :: t -> if h = card then acc :: t else remove_first_of_dup t card (h::acc)
+  | h :: t ->
+      if h = card then acc @ t else remove_first_of_dup t card (h :: acc)
 
-let remove_card player card = 
+let remove_card player card =
   if not (List.mem card player.hand) then raise (CardNotInHand card)
-  else let new_hand = remove_first_of_dup player.hand card [] in
-  {player with hand = new_hand}
+  else
+    let new_hand = remove_first_of_dup player.hand card [] in
+    { player with hand = new_hand }
