e5087564adbdc314e913e93c79963fe1cbdba69f
ysk27 <ysk27@cornell.edu>
2021-04-29 15:42:50 -0400

    added buffer between turns

--- main.ml
+++ main.ml
@@ -16,51 +16,6 @@ let check_quit () =
       exit 0
   | a -> int_of_string_opt a
 
-let to_string_color c =
-  let cc = color c in
-  match cc with
-  | R -> ANSITerminal.red
-  | G -> ANSITerminal.green
-  | B -> ANSITerminal.blue
-  | Y -> ANSITerminal.yellow
-  | ANY -> ANSITerminal.white
-
-(** [to_card_face c] is the face of the card [c]. *)
-let to_card_face c =
-  let ac = actions c in
-  if ac.skip = true then "ⓧ"
-  else if ac.reverse = true then "R"
-  else if fst ac.swap = true then "↔"
-  else if ac.change_color = true then
-    let pc = draw_penalty c in
-    if pc = 0 then "C" else "+4"
-  else
-    let pc = draw_penalty c in
-    if pc = 0 then string_of_int (Option.get (Card.digit c)) else "+2"
-
-let print_card card =
-  let c_color =
-    match color card with
-    | R -> "Red"
-    | G -> "Green"
-    | B -> "Blue"
-    | Y -> "Yellow"
-    | ANY -> "Black"
-  in
-  let face = to_card_face card in
-  "( " ^ c_color ^ ", " ^ face ^ " )"
-
-let cards_str cards =
-  List.fold_left
-    (fun str card -> (print_card card, card) :: str)
-    [] cards
-
-let rec print_cards = function
-  | [] -> ()
-  | h :: t ->
-      ANSITerminal.print_string [ to_string_color (snd h) ] (fst h ^ " ");
-      print_cards t
-
 let fail_str = "\nTry again.\n"
 
 let illegal_card = "\nYou can't play that card. Try another.\n"
@@ -99,15 +54,13 @@ let play_game players =
     let cur_player_hand = player_hand cur_player in
     print_string ("It is " ^ name cur_player ^ "'s turn.\n");
     print_string "The top card is:\n\n";
-    pp_cards [top_card g] false;
-    (**print_cards [ (print_card (top_card g), top_card g) ];*)
+    pp_cards [ top_card g ] false;
     print_string
       ("\nThe current stack penalty is "
       ^ string_of_int (stack_penalty g)
       ^ ".\n");
     print_string "Your cards are:\n\n";
     pp_cards cur_player_hand true;
-    (**print_cards (cards_str cur_player_hand);*)
     print_endline
       "\n\
        Type in the index of the card you wish to play (starting from \
@@ -121,8 +74,7 @@ let play_game players =
     | Some n ->
         if n > -2 && n < List.length cur_player_hand then (
           let play_card =
-            if n = -1 then None
-            else Some (List.nth cur_player_hand n)
+            if n = -1 then None else Some (List.nth cur_player_hand n)
           in
           match play play_card g with
           | Illegal ->
@@ -131,12 +83,17 @@ let play_game players =
               game_loop g
           | Legal next_g ->
               ignore (Sys.command "clear");
+              print_string
+                ("It is "
+                ^ name (current_player next_g)
+                ^ "'s turn. Enter anything to continue.\n");
+              read_line ();
+              ignore (Sys.command "clear");
               format_card g next_g play_card
           | GameOver winner ->
               print_string ("\n" ^ name winner ^ " wins!\n\n");
               exit 0)
-        else 
-        ignore (Sys.command "clear");  
+        else ignore (Sys.command "clear");
         print_string fail_str;
         game_loop g
   in
