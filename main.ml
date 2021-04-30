44a2fb308949a6fc00875b0c927914052801d584
ysk27 <ysk27@cornell.edu>
2021-04-29 21:56:10 -0400

    added computer support

--- main.ml
+++ main.ml
@@ -1,13 +1,23 @@
 open State
 open Card
 open Player
+open Computer
 
-(** [create_players \[\] n] is a list of [n] players.*)
-let rec create_players players = function
-  | 0 -> players
-  | n ->
-      let player = create ("Player " ^ string_of_int n) false in
-      create_players (player :: players) (n - 1)
+(** [create_players num_real num_computer] is a list of
+    [num_real + num_computer] players, where [num_computer] are
+    computers.*)
+let create_players players num_real num_computer =
+  let rec help lst cpus = function
+    | 0 -> lst
+    | n ->
+        let player =
+          if cpus then create ("Computer " ^ string_of_int n) true
+          else create ("Player " ^ string_of_int n) false
+        in
+        help (player :: lst) cpus (n - 1)
+  in
+  let people = help [] true num_computer in
+  help people false num_real
 
 let player_hand_info player =
   name player ^ " has "
@@ -60,7 +70,9 @@ let play_game players =
   let start_state = init_state standard_cards players in
   let rec game_loop g =
     let format_card gst next_gst = function
-      | None -> game_loop next_gst
+      | None ->
+          buffer next_gst;
+          game_loop next_gst
       | Some c ->
           if color c = ANY then
             let new_c = change_color c (select_color ()) in
@@ -76,46 +88,65 @@ let play_game players =
           game_loop next_gst
     in
     let cur_player = current_player g in
-    let cur_player_hand = player_hand cur_player in
-    print_string ("It is " ^ name cur_player ^ "'s turn.\n");
-    print_string "The top card is:\n\n";
-    pp_cards [ top_card g ] false;
-    print_string
-      ("\nThe current stack penalty is "
-      ^ string_of_int (stack_penalty g)
-      ^ ".\n");
-    print_string (player_hands_info cur_player g);
-    print_string "\nYour cards are:\n\n";
-    pp_cards cur_player_hand true;
-    print_endline
-      "\n\
-       Type in the index of the card you wish to play (starting from \
-       0). If you do not have a card to play, type -1.\n";
-    print_string "> ";
-    match check_quit () with
-    | None ->
-        clear ();
-        print_string fail_str;
-        game_loop g
-    | Some n ->
-        if n > -2 && n < List.length cur_player_hand then (
-          let play_card =
-            if n = -1 then None else Some (List.nth cur_player_hand n)
-          in
-          match play play_card g with
-          | Illegal ->
-              clear ();
-              print_string illegal_card;
-              game_loop g
-          | Legal next_g ->
-              clear ();
-              format_card g next_g play_card
-          | GameOver winner ->
-              print_string ("\n" ^ name winner ^ " wins!\n\n");
-              exit 0)
-        else clear ();
-        print_string fail_str;
-        game_loop g
+    if is_cpu cur_player then (
+      let cpu_card = action g in
+      let changed_gst =
+        if
+          Option.is_some cpu_card
+          && (actions (Option.get cpu_card)).change_color
+        then
+          change_current_players_hand
+            (change_color (Option.get cpu_card) ANY)
+            (Option.get cpu_card) g
+        else g
+      in
+      match play cpu_card changed_gst with
+      | Legal new_gst -> format_card changed_gst new_gst cpu_card
+      | Illegal -> print_string "The computer made an error!\n\n"
+      | GameOver _ ->
+          print_string "The computer wins...\n\n";
+          exit 0)
+    else
+      let cur_player_hand = player_hand cur_player in
+      print_string ("It is " ^ name cur_player ^ "'s turn.\n");
+      print_string "The top card is:\n\n";
+      pp_cards [ top_card g ] false;
+      print_string
+        ("\nThe current stack penalty is "
+        ^ string_of_int (stack_penalty g)
+        ^ ".\n");
+      print_string (player_hands_info cur_player g);
+      print_string "\nYour cards are:\n\n";
+      pp_cards cur_player_hand true;
+      print_endline
+        "\n\
+         Type in the index of the card you wish to play (starting from \
+         0). If you do not have a card to play, type -1.\n";
+      print_string "> ";
+      match check_quit () with
+      | None ->
+          clear ();
+          print_string fail_str;
+          game_loop g
+      | Some n ->
+          if n > -2 && n < List.length cur_player_hand then (
+            let play_card =
+              if n = -1 then None else Some (List.nth cur_player_hand n)
+            in
+            match play play_card g with
+            | Illegal ->
+                clear ();
+                print_string illegal_card;
+                game_loop g
+            | Legal next_g ->
+                clear ();
+                format_card g next_g play_card
+            | GameOver winner ->
+                print_string ("\n" ^ name winner ^ " wins!\n\n");
+                exit 0)
+          else clear ();
+          print_string fail_str;
+          game_loop g
   in
   game_loop start_state
 
@@ -126,19 +157,27 @@ let main () =
      Welcome to Uno. \n\
      Type 'Quit' at any time to quit the game.\n\n";
   let rec getPlayers u =
+    let restart () =
+      clear ();
+      print_string fail_str;
+      getPlayers ()
+    in
     print_endline
       "Type in the number of people that want to play the game.\n";
     print_string "> ";
     match check_quit () with
-    | None ->
-        clear ();
-        print_string fail_str;
-        getPlayers ()
-    | Some n ->
-        if n >= 2 then n |> create_players [] |> play_game
-        else (
-          print_string fail_str;
-          getPlayers ())
+    | None -> restart ()
+    | Some n -> (
+        print_endline
+          "How many computer opponents do you want? (In addition to \
+           the earlier number).\n";
+        print_string "> ";
+        match check_quit () with
+        | None -> restart ()
+        | Some c ->
+            if n + c >= 2 && n > 0 then
+              c |> create_players [] n |> play_game
+            else restart ())
   in
   getPlayers ()
 
