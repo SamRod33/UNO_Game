997e300a0ca7045f97b77b2a7dec5192ac2c492f
Keri Tenerowicz <kmt225@cornell.edu>
2021-05-14 14:47:05 -0400

    opens window

--- init.ml
+++ init.ml
@@ -1,8 +1,18 @@
 open Graphics
 
 ;;
-Graphics.open_graph "640x480"
+Graphics.open_graph " 800x680"
 
 let height = Graphics.size_y
 
 let width = Graphics.size_x
+
+let open_window =
+  try
+    while true do
+      ()
+    done
+  with _ -> Graphics.clear_graph ()
+
+;;
+open_window
