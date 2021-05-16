3ae8492c93e233fb5a88048fc67e08d2c0ce0765
Keri Tenerowicz <kmt225@cornell.edu>
2021-05-16 18:33:46 -0400

    added packages

--- window.ml
+++ window.ml
@@ -1,4 +1,6 @@
 open Graphics
+open Images
+open Png
 
 ;;
 Graphics.open_graph " 800x680"
@@ -18,17 +20,37 @@ let open_window =
     done
   with _ -> Graphics.clear_graph ()
 
-let upload_img = ()
+(* most of this is very similar to pokemon, must change *)
+let img_arr img_t =
+  let graph_arr =
+    match img_t with
+    | Rgba32 bits ->
+        let h = bits.Rgba32.height in
+        let w = bits.Rgba32.width in
+        Array.init h (fun heights ->
+            Array.init w (fun widths ->
+                let { color = { r; g; b }; alpha = tr } =
+                  Rgba32.unsafe_get bits widths heights
+                in
+                rgb r g b))
+    | _ -> failwith "never"
+  in
+  let list_img = Array.(map Array.to_list graph_arr |> to_list) in
+  let rec nappend a b n = nappend (Array.append a [| b |]) b (n - 1) in
+  List.fold_left
+    (fun acc row ->
+      nappend acc
+        (List.fold_left
+           (fun racc color -> nappend racc color 1)
+           [||] row)
+        1)
+    [||] list_img
 
-let draw_img img x y (*scale is_grid*) =
-  let g = img |> Graphics.make_image in
-  (*let coord_scaling = if is_grid then scale * px_size else scale in*)
-  Graphics.draw_image g x y
-
-let prepare_draw dir fn x y =
-  let img = Png.load (dir ^ fn ^ ".png") [] in
-  draw_img img x y
+let upload_img folder file x y =
+  let img = Png.load (folder ^ file ^ ".png") [] in
+  let draw = img |> img_arr |> Graphics.make_image in
+  Graphics.draw_image draw x y
 
 ;;
 open_window;
-prepare_draw "card" "Green"
+upload_img "card" "Green"
