228772281e5869f75a213902b2ed9a5f75f211d2
Keri Tenerowicz <kmt225@cornell.edu>
2021-05-18 18:51:04 -0400

    computer tests uncommented

---
+++ full_test_suite.ml
@@ -0,0 +1,14 @@
+open OUnit2
+
+let test_suites =
+  List.flatten
+    [
+      Card_test.card_suite;
+      Player_test.player_suite;
+      Computer_test.computer_suite;
+      State_test.state_suite;
+    ]
+
+let full_suite = "Testing entire suite" >::: test_suites
+
+let _ = run_test_tt_main full_suite
