c09bbb32aaa8df2d0a2955133a3fcb6122558698
Oluwatise Alatise <ofa2@cornell.edu>
2021-05-21 22:04:30 -0400

    Moved keys to constants.

--- constants.ml
+++ constants.ml
@@ -3,3 +3,15 @@ let width = "1024"
 let height = "768"
 
 let card_dir = "card/"
+
+let quit_key = 'q'
+
+let left_key = 'a'
+
+let right_key = 'd'
+
+let down_key = 's'
+
+let up_key = 'w'
+
+let draw_key = ' '
--- window.ml
+++ window.ml
@@ -3,6 +3,14 @@ open Constants
 open Images
 open Png
 
+let start_x = ref 240
+
+let start_y = ref 240
+
+let selected_outline_x = ref 300
+
+let selected_outline_y = ref 420
+
 let open_window =
   open_graph (" " ^ width ^ "x" ^ height);
   set_window_title "Uno Game by TKYS"
@@ -17,7 +25,8 @@ open_window;
 set_color (rgb 0 0 0);
 draw_rect 0 0 (int_of_string width) (int_of_string height);
 fill_rect 0 0 (int_of_string width) (int_of_string height);
-upload_img card_dir "Green" 250 250
+upload_img card_dir "Green" 250 250;
+set_color (rgb 248 218 39)
 
 ;;
 try
@@ -26,6 +35,14 @@ try
       wait_next_event [ Mouse_motion; Button_down; Key_pressed ]
     in
     synchronize ();
-    if st.key = 'q' then raise Exit
+    if st.key = quit_key then raise Exit
+    else if st.key = right_key then (
+      set_color (rgb 0 0 0);
+      draw_rect (!start_x - 400) !start_y !selected_outline_x
+        !selected_outline_y;
+      set_color (rgb 248 218 39);
+      draw_rect !start_x !start_y !selected_outline_x
+        !selected_outline_y;
+      start_x := !start_x + 400 )
   done
 with Exit -> ()
