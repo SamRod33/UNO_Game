eb4176c817640cba404ed584cb97443d2e137b14
Oluwatise Alatise <ofa2@cornell.edu>
2021-05-23 16:28:06 -0400

    fixed minor error in window_intermission.

--- window_intermission.ml
+++ window_intermission.ml
@@ -41,9 +41,8 @@ let change_color_phase st g =
   else if st.key = _CONFIRM_KEY then failwith "hi"
   else open_window;
   draw_intermission_window ();
-  display_player_num
-    (id (current_player g))
-    (600, 520) draw_cards (top_card g) (105, 101) card_space
+  display_player_num (id (current_player g)) (600, 520);
+  draw_cards [ top_card g ] (105, 101) card_space
 
 ;;
 open_window;
