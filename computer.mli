fcb3db7b89cb2dae79b4d8ad41abe13a7a9795e2
Keri Tenerowicz <kmt225@cornell.edu>
2021-05-24 08:03:12 -0400

    computer outputs 2 card tuple, og then color/id added

--- computer.mli
+++ computer.mli
@@ -2,11 +2,6 @@
 
     @author Keri Tenerowicz (kmt225) *)
 
-(** [basic_action g] is the first playable from the left in the
-    computer's hand in state [g]. None is returned if the cpu wants to
-    draw a card. *)
-val basic_action : State.t -> Card.t option
-
 (** [action g] is the card the computer will play based on a give state
     [g]. None is returned if the cpu wants to draw a card. *)
-val action : State.t -> Card.t option
+val action : State.t -> Card.t option * Card.t option
