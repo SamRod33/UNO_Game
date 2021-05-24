2dbc1158d64763c00515fdea528bbc5aaeaab3d6
Keri Tenerowicz <kmt225@cornell.edu>
2021-05-24 17:34:30 -0400

    added specs to window swap player

--- WindowSwapPlayer.ml
+++ WindowSwapPlayer.ml
@@ -34,8 +34,6 @@ let player_selected_idx = ref 0
 (* [swap_player_cards g] is the number of swap cards shown. *)
 let swap_player_cards g = List.length (players g) - 1
 
-(*****************************************************************************)
-
 (* [stid_handl_list g] is the list of player ids and their corresponding
    number of cards in game [g]. *)
 let stid_handl_list g =
@@ -45,8 +43,8 @@ let stid_handl_list g =
   in
   List.map (fun p -> (id p, List.length (player_hand p))) other_player
 
-(* [display_hand_amt i (x,y)] is the number [i] drawn using GUI images
-   at position [x], [y]. *)
+(* [display_hand_amt i (x, y)] is the number [i] drawn using GUI images
+   at position [(x, y)]. *)
 let rec display_hand_amt i (x, y) =
   if i < 10 then upload_img _TEXT_DIR (string_of_int i ^ "_med") x y
   else if i < 100 then (
@@ -55,15 +53,15 @@ let rec display_hand_amt i (x, y) =
     upload_img _TEXT_DIR (string_of_int fst_dig ^ "_med") x y;
     display_hand_amt snd_dig (x + num_space, y))
 
-(* [draw_player_hand_card c (x,y)] draws the card [c] for the player's
-   hand in the window starting at position [x], [y]. *)
+(* [draw_player_hand_card c (x, y)] draws the card [c] for the player's
+   hand in the window starting at position [(x, y)]. *)
 let draw_player_hand_card c (x, y) =
   let id, amt = c in
   upload_img _ASSET_DIR ("player_hand_" ^ string_of_int id) x y;
   display_hand_amt amt (x + num_offset_x, y + num_offset_y)
 
-(** [draw_cards cards pos] draws [cards] starting at [pos]. Ensures
-    spacing between each card. *)
+(** [draw_cards g (x,y)] draws for the opponent players in game [g]
+    starting at [(x,y)]. Ensures spacing between each card. *)
 let rec draw_cards g (x, y) =
   let rec drawing l (x, y) =
     let p_space_x, p_space_y = swap_space in
@@ -84,8 +82,8 @@ let draw_swap_player_screen g =
   upload_img _TEXT_DIR "Choose a player" swap_txt_pos_x swap_txt_pos_y;
   draw_cards g (cards_swap_start_pos_x, cards_swap_start_pos_y)
 
-(* [guiid_id ply_lst acc] is a list of integer * integer tuples that
-   contain the the gui id and player id of each player in ply_lst. *)
+(* [guiid_id g] is a list of integer * integer tuples that contain the
+   the gui id and player id of each player in game [g]. *)
 let guiid_id g =
   let rec tup_creator ply_lst acc =
     match ply_lst with
@@ -109,22 +107,20 @@ let rec print_id_lst = function
    player's id [gui_idx] from the swap window. *)
 let get_id gui_idx g = string_of_int (List.assoc gui_idx (guiid_id g))
 
-(** [move op limit space color_a color_b] draws the new selection. *)
+(** [move op space color_a color_b] draws the new selection. *)
 let move op space color_a color_b =
   highlight_selection color_a color_b (op 0 space) !outline_pos_x
     !outline_pos_y outline_width outline_height;
   outline_pos_x := op !outline_pos_x space
 
-(* [swap_player_phase st] launches the swap player window phase. *)
+(* [swap_player_phase st g] launches the swap player window phase for
+   game [g]. *)
 let swap_player_phase st g =
   if st.key = _QUIT_KEY then exit 0
   else if st.key = _CONFIRM_KEY then raise Exit
   else if st.key = _RIGHT_KEY then
-    if
-      !outline_pos_x
-      >= (swap_player_cards g - 1 (*maybe not need -1*))
-         * fst swap_space
-    then ()
+    if !outline_pos_x >= (swap_player_cards g - 1) * fst swap_space then
+      ()
     else (
       move ( + ) (fst swap_space) _GOLD _BLACK;
       player_selected_idx := !player_selected_idx + 1)
@@ -270,6 +266,8 @@ let g = t_test std_deck std_deck 0 blue6 [ p5; p2; p1; p4 ]
 
 (******************************************************************************)
 
+(* [swap_player_win g] is the id of the selected player to swap with in
+   game [g]. *)
 let swap_player_win (*()*) g =
   print_id_lst (guiid_id g);
   print_string "\nfinal id to swap hands with: ";
