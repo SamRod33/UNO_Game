fd771eeccaf56684cdae71e89311256acf22f86c
Sam <sar325@cornell.edu>
2021-05-23 23:54:58 -0400

    add spec to intro_win

--- WindowIntro.ml
+++ WindowIntro.ml
@@ -6,6 +6,7 @@ open WindowGui
 
 let intro_txt_x, intro_txt_y = (logo_pos_x, logo_pos_y - 100)
 
+(** [intro_win] displays the Intro Window to the UNO Game. *)
 let intro_win =
   upload_img _ASSET_DIR "Start_Game" 0 0;
   try
